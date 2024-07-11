6f786bc5d78e33d78b4a94f0a9940d1d3446661f
Misy <78818724+zrfli@users.noreply.github.com>
2024-07-11 17:04:43 +0300

    bug fixed

--- main/dashboard/index.php
+++ main/dashboard/index.php
@@ -5,7 +5,8 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/src/database/db.php';
 
 if (!$auth -> isLoggedIn()){ header('Location: /login'); exit(); }
 
-include_once 'inc/head/content.phtml'; 
+include_once 'inc/head/content.phtml';
+require_once $_SERVER['DOCUMENT_ROOT'].'/src/functions/includeAsset.php';
 include_once 'inc/header/content.phtml'; 
 include_once 'inc/sidebar/content.phtml'; 
 include_once 'inc/index_base/content.phtml';
