2be86cd84dfbcbf1108213030a3511c002ba8a8f
Misy <78818724+zrfli@users.noreply.github.com>
2024-07-15 18:37:22 +0300

    Update languageController.php

--- src/language/languageController.php
+++ src/language/languageController.php
@@ -5,8 +5,7 @@ $langs = ['turkish', 'english'];
 
 if (!isset($_COOKIE['lang']) OR !in_array($_COOKIE['lang'], $langs)) { setcookie('lang', 'turkish', time() + 60 * 60 * 24 * 365, '/'); } 
 
-require_once match ($_COOKIE['lang']) {
+require_once match ($_COOKIE['lang'] ?? 'turkish') {
     'turkish' => $_SERVER['DOCUMENT_ROOT'].'/src/language/turkish.php',
-    'english' => $_SERVER['DOCUMENT_ROOT'].'/src/language/english.php',
-    default => $_SERVER['DOCUMENT_ROOT'].'/src/language/turkish.php'
+    'english' => $_SERVER['DOCUMENT_ROOT'].'/src/language/english.php'
 };
