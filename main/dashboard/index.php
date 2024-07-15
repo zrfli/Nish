2d9cd9e362d1a060ed57b4560254ac747cdba81b
Misy <78818724+zrfli@users.noreply.github.com>
2024-07-15 18:02:08 +0300

    Update index.php

--- main/dashboard/index.php
+++ main/dashboard/index.php
@@ -5,9 +5,9 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/src/database/db.php';
 
 if (!$auth -> isLoggedIn()) { header('Location: /login'); exit(); }
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/inc/head/content.phtml';
+require_once 'inc/head/content.phtml';
 require_once $_SERVER['DOCUMENT_ROOT'].'/src/functions/includeAsset.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/inc/header/content.phtml'; 
-require_once $_SERVER['DOCUMENT_ROOT'].'/inc/sidebar/content.phtml'; 
-require_once $_SERVER['DOCUMENT_ROOT'].'/inc/index_base/content.phtml';
-require_once $_SERVER['DOCUMENT_ROOT'].'/inc/js/content.phtml';
+require_once 'inc/header/content.phtml'; 
+require_once 'inc/sidebar/content.phtml'; 
+require_once 'inc/index_base/content.phtml';
+require_once 'inc/js/content.phtml';
